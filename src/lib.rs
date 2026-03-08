da35f4f8adbaa7bfd8191da7df33bfdf0536ca64
Serissa <serissacodecat@proton.me>
2026-03-08 16:26:27 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+pub mod utils;
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum Client {
+    Join {
+        chat_name: Arc<String>
+    }, 
+    Post {
+        chat_name: Arc<String>,
+        message: Arc<String>
+    }
+    
+}
+
+pub enum Server {
+    Message {
+        chat_name: Arc<String>, 
+        msg: Arc<String>,   
+    },
+    Error(String)
+}
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
